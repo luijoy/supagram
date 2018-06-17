5bdb82e5fdf57c19c841d87b1a2717ec94dd31ee
Lui Kohl <lui.kohl@gmail.com>
2018-06-17 14:38:41 +0200

    removed files

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
-  resources :posts
-
-end
