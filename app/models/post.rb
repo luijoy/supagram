adfd2639e3c7e6feb7bf8c6a622bdadf5283b7ca
Lui Kohl <lui.kohl@gmail.com>
2018-06-18 11:28:42 +0200

    basic comments are working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
+  validates :user_id, presence: true
   validates :image, presence: true
-
+  belongs_to :user
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
