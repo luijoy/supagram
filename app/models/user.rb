c952e7254c4f54f0ede35ed1a1146eaa6f67c8dd
Lui Kohl <lui.kohl@gmail.com>
2018-06-22 12:10:23 +0200

    created profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
